ea4d5463a8c00a160a2820a8732cc15e5c8313bc
Hans Neve <hans.neve@gmail.com>
2025-02-21 12:51:37 +0100

    Oppdater main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,21 +1,3 @@
-bluetooth.onBluetoothConnected(function () {
-    basic.showLeds(`
-        . . . . .
-        # . . . .
-        . # . . .
-        . . # . #
-        . . . # .
-        `)
-})
-bluetooth.onBluetoothDisconnected(function () {
-    basic.showLeds(`
-        # . . . #
-        . # . # .
-        . . # . .
-        . # . # .
-        # . . . #
-        `)
-})
 input.onButtonPressed(Button.A, function () {
     if (run == 0) {
         run = 1
@@ -25,12 +7,16 @@ input.onButtonPressed(Button.A, function () {
 })
 let run = 0
 keyboard.startKeyboardService()
+let now = control.millis()
 basic.forever(function () {
     if (run == 0) {
         led.plot(0, 0)
+        now = control.millis()
     } else {
         led.unplot(0, 0)
-        keyboard.sendString("e")
-        basic.pause(2000)
+        if (control.millis() > now + 2000) {
+            keyboard.sendString("e")
+            now = control.millis()
+        }
     }
 })
